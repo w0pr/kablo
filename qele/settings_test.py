97def43347d119f82a6ddb66244d543b1fcf8c6e
monodo <olivier.monod@yverdon-les-bains.ch>
2024-02-09 14:40:07 +0100

    settings for tests

---
+++ qele/settings_test.py
@@ -0,0 +1,13 @@
+from .settings import *
+
+# Disable Axes for tests
+# TODO: enable it later
+AXES_ENABLED = False
+# Speed up tests by using a fast password hasher
+# https://docs.djangoproject.com/en/3.0/topics/testing/overview/#speeding-up-the-tests
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+SITE_ID = 1
+SITE_DOMAIN = "localhost"
