fc94e960aaadc16142504b41b943307405e0cdc2
Damiano Lombardi <damiano@opengis.ch>
2024-02-21 16:29:34 +0100

    Models draft

--- kablo/apps/accounts/models.py
+++ kablo/apps/accounts/models.py
@@ -1,1 +1,21 @@
-# Create your models here.
+from django.contrib.gis.db import models
+
+
+class NetworkSegment(models.Model):
+    geom = models.LineStringField(srid=2056)
+
+
+class Trasse(models.Model):
+    networkSegments = models.ManyToManyField(NetworkSegment)
+
+
+class Tube(models.Model):
+    networkSegments = models.ManyToManyField(NetworkSegment)
+
+
+class Station(models.Model):
+    position = models.PointField(srid=2056)
+
+
+class Cable(models.Model):
+    tubes = models.ManyToManyField(Tube)
