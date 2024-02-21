dc16bc67fc548a26d1054b164cde470ee3d4daa5
Damiano Lombardi <damiano@opengis.ch>
2024-02-21 19:20:12 +0100

    Trasse to track (#12)

--- kablo/apps/network/admin.py
+++ kablo/apps/network/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Cable, NetworkSegment, Station, Trasse, Tube
+from .models import Cable, NetworkNode, NetworkSegment, Station, Track, Tube
 
+admin.site.register(NetworkNode)
 admin.site.register(NetworkSegment)
-admin.site.register(Trasse)
+admin.site.register(Track)
 admin.site.register(Tube)
 admin.site.register(Station)
 admin.site.register(Cable)
--- kablo/apps/network/models.py
+++ kablo/apps/network/models.py
@@ -1,11 +1,21 @@
 from django.contrib.gis.db import models
 
 
+class NetworkNode(models.Model):
+    geom = models.PointField(srid=2056)
+
+
 class NetworkSegment(models.Model):
     geom = models.LineStringField(srid=2056)
+    network_node_start = models.ForeignKey(
+        NetworkNode, related_name="network_node_start", on_delete=models.CASCADE
+    )
+    network_node_end = models.ForeignKey(
+        NetworkNode, related_name="network_node_end", on_delete=models.CASCADE
+    )
 
 
-class Trasse(models.Model):
+class Track(models.Model):
     networkSegments = models.ManyToManyField(NetworkSegment)
 
 
@@ -19,3 +29,9 @@ class Station(models.Model):
 
 class Cable(models.Model):
     tubes = models.ManyToManyField(Tube)
+    station_start = models.ForeignKey(
+        Station, related_name="station_start", on_delete=models.CASCADE
+    )
+    station_end = models.ForeignKey(
+        Station, related_name="station_end", on_delete=models.CASCADE
+    )
