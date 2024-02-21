da562034387aa41fb91805fb1d29b3569d6c31c6
monodo <olivier.monod@yverdon-les-bains.ch>
2024-02-21 19:21:08 +0100

    use custom user model

--- kablo/apps/accounts/admin.py
+++ kablo/apps/accounts/admin.py
@@ -1,1 +1,5 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from kablo.apps.accounts.models import User
+
+admin.site.register(User, UserAdmin)
