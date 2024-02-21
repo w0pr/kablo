b9e24aee2ea3e2affa24a9832eaddc03b64a502b
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-02-21 19:19:57 +0100

    add gh workflow (#9)
    * add gh workflow
    
    * remove django.db.connection.ensure_connection
    
    * health condition
    
    * push by default
    
    * use action cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# Ensuire we have connection to the database.
-# If not will, the container will fail and restart.
-python3 manage.py shell -c "import django; django.db.connection.ensure_connection();"
-
 # On PROD, we run migrations at startup unless explicitly disabled.
 # If disabled, this command must be run manually for the application to function correctly after a model update.
 if [ "$ENV" == "PROD" ] && [ "${DISABLE_MIGRATION_SCRIPT_ON_PRODUCTION}" != "true" ]; then
