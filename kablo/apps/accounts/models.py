2d1f33c8b338f05db04abe90990568c6caf166d0
monodo <olivier.monod@yverdon-les-bains.ch>
2024-02-21 18:02:57 +0100

    wip

--- kablo/apps/accounts/models.py
+++ kablo/apps/accounts/models.py
@@ -1,21 +1,5 @@
-from django.contrib.gis.db import models
+# from django.contrib.auth.models import AbstractUser
 
 
-class NetworkSegment(models.Model):
-    geom = models.LineStringField(srid=2056)
-
-
-class Trasse(models.Model):
-    networkSegments = models.ManyToManyField(NetworkSegment)
-
-
-class Tube(models.Model):
-    networkSegments = models.ManyToManyField(NetworkSegment)
-
-
-class Station(models.Model):
-    position = models.PointField(srid=2056)
-
-
-class Cable(models.Model):
-    tubes = models.ManyToManyField(Tube)
+# class User(AbstractUser):
+#     pass
