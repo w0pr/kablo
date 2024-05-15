e549183c4e9e79f52228a23535c596464fb4d1ac
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-05-15 11:24:08 +0200

    reload script + cleanup

--- scripts/migrate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Migration script (database migrations, collectstatic, etc.). This must be run
-# on startup if changes have been made to the models, the static files, etc.
-
-set -e
-
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
--- scripts/reset-migrations.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-
-docker compose exec kablo python manage.py makemigrations
-docker compose exec kablo python manage.py migrate
