d2effbedc95e4f9201ea023ef68bae11edf7c480
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-05-16 15:08:50 +0200

    basic profile plot

--- kablo/network/urls.py
+++ kablo/network/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile/<slug:section_id>/", views.section_profile),
-    path("profile/<slug:section_id>/<int:distance>/", views.section_profile),
+    path("profile/<slug:_format>/<slug:section_id>/", views.section_profile),
+    path(
+        "profile/<slug:_format>/<slug:section_id>/<int:distance>/",
+        views.section_profile,
+    ),
 ]
--- kablo/network/views.py
+++ kablo/network/views.py
@@ -1,16 +1,36 @@
+import plotly.graph_objects as go
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from kablo.network.models import Section
 
 
-def section_profile(request, section_id, distance: int = 0):
+def _min(current, offset, offset_optional, diameter):
+    offset_min = min(offset, offset_optional or offset)
+    if not current:
+        return offset_min - diameter / 2
+    return min(current, offset_min - diameter / 2)
+
+
+def _max(current, offset, offset_optional, diameter):
+    offset_max = max(offset, offset_optional or offset)
+    if not current:
+        return offset_max + diameter / 2
+    return max(current, offset_max + diameter / 2)
+
+
+def section_profile(request, section_id, distance: int = 0, _format="json"):
 
     section = get_object_or_404(Section, id=section_id)
 
     qs = section.tubesection_set.all()
 
     tubes = []
+
+    x_min = None
+    x_max = None
+    z_min = None
+    z_max = None
 
     for tube_section in qs:
 
@@ -47,4 +67,57 @@ def section_profile(request, section_id, distance: int = 0):
         }
         tubes.append(tube)
 
-    return JsonResponse({"section": section_id, "tubes": tubes})
+        x_min = _min(
+            x_min,
+            tube_section.offset_x,
+            tube_section.offset_x_2,
+            tube_section.tube.diameter,
+        )
+        x_max = _max(
+            x_max,
+            tube_section.offset_x,
+            tube_section.offset_x_2,
+            tube_section.tube.diameter,
+        )
+        z_min = _min(
+            z_min,
+            tube_section.offset_z,
+            tube_section.offset_z_2,
+            tube_section.tube.diameter,
+        )
+        z_max = _max(
+            z_max,
+            tube_section.offset_z,
+            tube_section.offset_z_2,
+            tube_section.tube.diameter,
+        )
+
+    if format == "json":
+        return JsonResponse({"section": section_id, "tubes": tubes})
+
+    else:
+        fig = go.Figure()
+
+        fig.update_xaxes(range=[x_min, x_max], zeroline=False)
+        fig.update_yaxes(range=[z_min, z_max])
+
+        for tube in tubes:
+            fig.add_shape(
+                type="circle",
+                xref="x",
+                yref="y",
+                x0=tube["pos"]["x"] - tube["diameter"] / 2,
+                x1=tube["pos"]["x"] + tube["diameter"] / 2,
+                y0=tube["pos"]["z"] - tube["diameter"] / 2,
+                y1=tube["pos"]["z"] + tube["diameter"] / 2,
+                line_color="Grey",
+            )
+
+        fig.update_yaxes(
+            scaleanchor="x",
+            scaleratio=1,
+        )
+
+        profile = fig.to_html()
+        context = {"profile": profile}
+        return render(request, "profile.html", context)
